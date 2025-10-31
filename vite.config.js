63b0d5acd0b1465c269dc0003f6193d346cd9a5d
mandi-stone <kabudon19@gmail.com>
2025-10-31 13:40:11 +0100

    deploy fixure

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,18 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['vue'],
+        },
+      },
+    },
+  },
+  base: './',
 })
